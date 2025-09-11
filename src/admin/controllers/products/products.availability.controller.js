9b13659937bd4f156a10d320e34717d8e0677431
Vitaliy Saichuk <v_saichuk@Vitaliys-MacBook-Pro.local>
2025-09-11 13:38:20 +0300

    Синхронізація ProductAvailability з переданим масивом: оновлення за _id, створення нових записів, видалення відсутніх; повернення актуального списку.

--- src/admin/controllers/products/products.availability.controller.js
+++ src/admin/controllers/products/products.availability.controller.js
@@ -35,19 +35,37 @@ export const update = async (req, res) => {
             return res.status(400).json(error.array());
         }
 
-        const availabilityData = req.body;
+        const availabilityData = Array.isArray(req.body) ? req.body : [];
 
-        const results = await Promise.all(
-            availabilityData.map(async (item) => {
-                if (item._id) {
-                    return ProductAvailability.findByIdAndUpdate(item._id, item, { new: true });
-                } else {
-                    return new ProductAvailability(item).save();
-                }
-            }),
-        );
+        const itemsWithId = availabilityData.filter((item) => item && item._id);
+        const itemsWithoutId = availabilityData.filter((item) => item && !item._id);
 
-        res.json({ success: true, data: results });
+        const updateOps = itemsWithId.map((item) => {
+            const { _id, ...updateFields } = item;
+            return {
+                updateOne: {
+                    filter: { _id },
+                    update: { $set: updateFields },
+                },
+            };
+        });
+
+        if (updateOps.length > 0) {
+            await ProductAvailability.bulkWrite(updateOps, { ordered: false });
+        }
+
+        let insertedDocs = [];
+        if (itemsWithoutId.length > 0) {
+            insertedDocs = await ProductAvailability.insertMany(itemsWithoutId, { ordered: false });
+        }
+
+        const keepIds = [...itemsWithId.map((i) => i._id), ...insertedDocs.map((doc) => doc._id)];
+
+        await ProductAvailability.deleteMany({ _id: { $nin: keepIds } });
+
+        const all = await ProductAvailability.find();
+
+        res.json({ success: true, data: all });
     } catch (err) {
         console.error(err);
         res.status(500).json({ success: false, message: 'Internal Server Error' });
